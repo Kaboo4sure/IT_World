cf60d97b9658c2adf98d640aa9fbb116097bdc49
Kaboo4sure <93238603+Kaboo4sure@users.noreply.github.com>
2024-05-12 14:21:36 -0500

    Update ITWorld.ps1

--- ITWorld.ps1
+++ ITWorld.ps1
@@ -1,1 +1,9 @@
+#echo "# Testing_2" >> README.md
+#git init
+#git add README.md
+#git branch -M main
+#git remote add origin git@github.com:Kaboo4sure/Testing_2.git
+#git push -u origin main
+
+
 write-host " This is now working"
